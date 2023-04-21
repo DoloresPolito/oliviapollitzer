c5e3d1d3bb512a3f822d7fe29bb794655d6fd7bc
Dolores Polito <dolorespolito@MacBook-Air-de-Dolores.local>
2023-04-21 13:08:50 -0300

    responsive pictures

--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -3,19 +3,18 @@ import "../App.css";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import styled from "styled-components";
-import picture1 from "../assets/pictures/1*.jpeg"
-import picture2 from "../assets/pictures/2*.jpeg"
-import picture3 from "../assets/pictures/3*.jpeg"
-import picture4 from "../assets/pictures/4*.jpeg"
-import picture5 from "../assets/pictures/pic3.jpg"
+import picture1 from "../assets/pictures/1*.jpeg";
+import picture2 from "../assets/pictures/2*.jpeg";
+import picture3 from "../assets/pictures/3*.jpeg";
+import picture4 from "../assets/pictures/4*.jpeg";
+import picture5 from "../assets/pictures/pic3.jpg";
 
-import picture6 from "../assets/pictures/pic2.jpg"
+import picture6 from "../assets/pictures/pic2.jpg";
 
 const InstaResume = () => {
   const animation1 = useAnimation();
   const { ref, inView } = useInView({ threshold: 0 });
   const [width, setWidth] = useState(window.innerWidth);
-
 
   useEffect(() => {
     if (inView) {
@@ -35,80 +34,24 @@ const InstaResume = () => {
     }
   }, [inView]);
 
-
-
   return (
     <InstaResumeSection>
       <div ref={ref}>
         <ResumeGallery animate={animation1}>
-          {width > 1000 ? (
-            <>
-           
-                <div >
-                  <img
-                    src={picture1}
-                    className="gallery-image"
-                    alt=""
-                    style={{ padding: "2px" }}
-                  />
-                </div>
-                <div >
-                  <img
-                    src={picture6}
-                    className="gallery-image"
-                    alt=""
-                    style={{ padding: "2px" }}
-                  />
-                </div>
-                <div >
-                  <img
-                    src={picture2}
-                    className="gallery-image"
-                    alt=""
-                    style={{ padding: "2px" }}
-                  />
-                </div>
-                <div >
-                  <img
-                    src={picture3}
-                    className="gallery-image"
-                    alt=""
-                    style={{ padding: "2px" }}
-                  />
-                </div>
-               
-        
-            </>
-          ) : (
-            <>
-          
-                <div >
-                  <img
-                    src={picture1}
-                    className="gallery-image"
-                    alt=""
-                    style={{ padding: "2px" }}
-                  />
-                </div>
-                <div >
-                  <img
-                    src={picture6}
-                    className="gallery-image"
-                    alt=""
-                    style={{ padding: "2px" }}
-                  />
-                </div>
-                <div >
-                  <img
-                    src={picture3}
-                    className="gallery-image"
-                    alt=""
-                    style={{ padding: "2px" }}
-                  />
-                </div>
-         
-            </>
-          )}
+          <>
+            <div>
+              <img src={picture1} className="gallery-image" alt="" />
+            </div>
+            <div>
+              <img src={picture6} className="gallery-image" alt="" />
+            </div>
+            <div>
+              <img src={picture2} className="gallery-image" alt="" />
+            </div>
+            <div>
+              <img src={picture3} className="gallery-image" alt="" />
+            </div>
+          </>
         </ResumeGallery>
       </div>
     </InstaResumeSection>
@@ -117,34 +60,48 @@ const InstaResume = () => {
 
 const InstaResumeSection = styled.div`
   width: 100%;
-  padding-top: 80px;
-  padding-bottom: 80px;
   display: flex;
   flex-direction: column;
   align-items: center;
   color: #5f5f67;
-  height: 450px;
+  height: auto;
+  padding-top: 100px;
+  padding-bottom: 100px;
   background-color: #faf9f8;
+  justify-content: center;
 
-  p {
-    width: 200px;
-    font-size: 17px;
-    font-family: "Montserrat", sans-serif;
-    letter-spacing: 1px;
-    font-weight: 500;
-    text-align: center;
-    padding-top: 5px;
-    padding-bottom: 15px;
-    line-height: 28px;
-    color: #5f5f67;
+  @media only screen and (max-width: 800px) {
+    padding-top: 50px;
+    padding-bottom: 50px;
   }
 `;
 
 const ResumeGallery = styled(motion.div)`
   display: flex;
   flex-direction: row;
-  height: 190px;
-  width: 950px;
+  width: 100%;
+  margin: 0 auto;
+  flex-wrap: wrap;
+  justify-content: center;
+  @media only screen and (max-width: 800px) {
+    width: 70%;
+  }
+  div {
+    height: 250px;
+    width: 250px;
+    @media only screen and (max-width: 1100px) {
+      height: 200px;
+      width: 200px;
+    }
+    img {
+      padding: 5px;
+
+      @media only screen and (max-width: 799px) {
+        padding: 10px;
+        
+      }
+    }
+  }
 `;
 
 export default InstaResume;
